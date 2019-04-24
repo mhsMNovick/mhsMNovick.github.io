f44d391d1d6633cd9af75b8c41d5764ac89fbe90
mhsMNovick <48356538+mhsMNovick@users.noreply.github.com>
2019-04-24 08:54:43 -0400

    Update sketch.js

--- Sound & Images/sketch.js
+++ Sound & Images/sketch.js
@@ -31,7 +31,7 @@ function draw() {
   if (hit===true){
 background(255,0,0);
 img.resize(400, 400);
-image(img,width/10,height/10);
+image(img,windowWidth/2,windowHeight/2);
 textSize(50);
 text('GAME OVER', 500, 100);
 textSize(25);
