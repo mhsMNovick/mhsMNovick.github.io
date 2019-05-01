9516bff47fe3d835d70cbca291780729be8afec7
mhsMNovick <48356538+mhsMNovick@users.noreply.github.com>
2019-05-01 10:03:33 -0400

    Update sketch.js

--- Descisions/sketch.js
+++ Descisions/sketch.js
@@ -1,11 +1,25 @@
-let random=('rock','paper','scissors')
+let choice=('rock','paper','scissors')
+let choices;
 
 function setup() {
   // put setup code here
   createCanvas(windowWidth,windowHeight);
+  background(100,250,234);
+  choices=random(choice);
   
 }
 
 function draw() {
   // put drawing code here
 }
+
+
+function keyPressed() {
+
+
+  if (keyCode === LEFT_ARROW) {
+    text('rock'windowWidth/2,windowHeight/2
+
+
+  }
+  }
