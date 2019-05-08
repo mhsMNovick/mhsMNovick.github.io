70536308775888fbe3f3e758841cb84031fd27ff
mhsMNovick <48356538+mhsMNovick@users.noreply.github.com>
2019-05-08 08:59:19 -0400

    Update sketch.js

--- Using Mouse Assignment Part2/sketch.js
+++ Using Mouse Assignment Part2/sketch.js
@@ -1,29 +1,37 @@
-let size=10
+  /* Using Mouse Part 2
+    Matthew Novick 
+    April 16, 2019
+    Revised: May 8, 2019
+  */
 
-function setup() {
+
+
+  let size=10
+
+  function setup() {
   // put setup code here
   createCanvas(1200,1000);
   background(100,20,240)
-}
+  }
 
 
-function draw() {
+  function draw() {
 
   }
   function mouseMoved(){
   if (mouseX) {
   fill(random(255),random(255),random(255));
   line(mouseX,mouseY, size,size);
-}
+  }
 
 
-}
+    }
 
-function mouseClicked(){
-if (size){
+  function mouseClicked(){
+  if (size){
   background(random(255),random(255),random(255))
-size=random(255)
+  size=random(255)
 
-}
+  }
 
-}
+    }
