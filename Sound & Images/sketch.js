76aef1051185acbddbf79d2b9dc77bf3811d8673
mhsMNovick <48356538+mhsMNovick@users.noreply.github.com>
2019-05-08 09:08:29 -0400

    Update sketch.js

--- Sound & Images/sketch.js
+++ Sound & Images/sketch.js
@@ -1,95 +1,102 @@
-let img;
-let mySound;
-let x=0;
-let x2=600;
-let y=300;
-let y2=300
-let size=50;
-
-
-let hit=false
+  /* Sound & Images assignment
+     Matthew Novick
+     April 24, 2019 
+     Revised: May 8, 2019 */
 
 
 
-let x1Speed=10;
+  let img;
+  let mySound;
+  let x=0;
+  let x2=600;
+  let y=300;
+  let y2=300
+  let size=50;
 
 
-function preload() {
-  img = loadImage('Rapslogo.png');
-  mySound = loadSound('dingdong.mp3'); 
-}
+  let hit=false
 
-function setup() {
-  // put setup code here
-  createCanvas(windowWidth,windowHeight);
- 
-}
 
-function draw() {
-  // put drawing code here
-  hit=collideRectRect(x,y,size,size,x2,y2,size,size);
 
-  print(hit);
+  let x1Speed=10;
 
-  if (hit===true){
-      mySound.setVolume(0.1);
-    mySound.play();
-background(255,0,0);
-img.resize(400, 400);
-image(img,width/2,height/2);
-textSize(50);
-text('GAME OVER', 500, 100);
-textSize(25);
-text('Press F5 to restart',525,125);
-x1Speed=0
 
+  function preload() {
+    img = loadImage('Rapslogo.png');
+    mySound = loadSound('dingdong.mp3'); 
+  }
+
+  function setup() {
+    // put setup code here
+    createCanvas(windowWidth,windowHeight);
 
   }
-  else {
-  background(0);
 
-fill(127);
-rect(x,y,size,size);
+  function draw() {
+    // put drawing code here
+    hit=collideRectRect(x,y,size,size,x2,y2,size,size);
 
-fill(255);
-if (keyIsDown(65)) {
-  x2 -= 5;
-}
+    print(hit);
 
-if (keyIsDown(68)) {
-  x2 += 5;
-}
-
-if (keyIsDown(87)) {
-  y2 -= 5;
-}
-
-if (keyIsDown(83)) {
-  y2 += 5;
-}
-rect(x2,y2,size,size);
+  if (hit===true){
+        mySound.setVolume(0.1);
+      mySound.play();
+  background(255,0,0);
+  img.resize(400, 400);
+  image(img,width/2,height/2);
+  textSize(50);
+  text('GAME OVER', 500, 100);
+  textSize(25);
+  text('Press F5 to restart',525,125);
+  x1Speed=0
 
 
-if (y2<0){
-  y2=0
-}
-else if (y2+size>=height){
+    }
+    else {
+    background(0);
 
-  y2=height-size
-}
+  fill(127);
+  rect(x,y,size,size);
 
-if(x2+size>=width){
+  fill(255);
+  if (keyIsDown(65)) {
+    x2 -= 5;
+  }
 
-  x2=width-size
-}
-else if (x2<=0){
-x2=0}
-}
+  if (keyIsDown(68)) {
+    x2 += 5;
+  }
 
-x+=x1Speed
-if(x+size>width || x<0){
-x1Speed*=-1;
+  if (keyIsDown(87)) {
+    y2 -= 5;
+  }
 
-}
+  if (keyIsDown(83)) {
+    y2 += 5;
+  }
+  rect(x2,y2,size,size);
 
-}
+
+  if (y2<0){
+    y2=0
+  }
+  else if (y2+size>=height){
+
+    y2=height-size
+  }
+
+  if(x2+size>=width){
+
+    x2=width-size
+  }
+  else if (x2<=0){
+  x2=0}
+  }
+
+  x+=x1Speed
+  if(x+size>width || x<0){
+  x1Speed*=-1;
+
+  }
+
+  }
