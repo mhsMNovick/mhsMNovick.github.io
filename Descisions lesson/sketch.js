6c87117e933bdf96c1995dfbdbcb6ca08ff1b975
mhsMNovick <48356538+mhsMNovick@users.noreply.github.com>
2019-05-08 09:14:06 -0400

    Update sketch.js

--- Descisions lesson/sketch.js
+++ Descisions lesson/sketch.js
@@ -1,20 +1,27 @@
-let choices=['rock','paper','scissors'];
-let choice;
-function setup() {
+  /* Decision Structure 
+     Matthew Novick
+     April 30, 2019
+     Revised: May 8, 2019 */
+
+
+
+  let choices=['rock','paper','scissors'];
+  let choice;
+  function setup() {
   // put setup code here
   createCanvas(800,600);
   background(100,250,234)
   choice=random(choices);
-}
+  }
 
-function draw() {
+  function draw() {
   // put drawing code here
   textSize(25);
   text('CPU',width/2+25,height/2-25);
   text('Player',width/2-100,height/2-25);
-}
+  }
 
-function keyPressed(){
+  function keyPressed(){
 
 
 
@@ -26,9 +33,9 @@ function keyPressed(){
     text('rock',width/2-100,height/2);
 
 
-}
+  }
 
-if (keyCode === RIGHT_ARROW) {
+  if (keyCode === RIGHT_ARROW) {
 
   background(100,250,234)
     textSize(25)
@@ -40,9 +47,9 @@ if (keyCode === RIGHT_ARROW) {
 
 
 
-}
+  }
 
-if (keyCode === UP_ARROW) {
+  if (keyCode === UP_ARROW) {
 
   background(100,250,234)
     textSize(25)
@@ -55,5 +62,5 @@ if (keyCode === UP_ARROW) {
 
 
 
-}
-}
+  }
+    }
